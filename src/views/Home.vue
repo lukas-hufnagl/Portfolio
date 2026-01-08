f62a6fd6394878174aa97a4502ed34240cfeefe3
lukas-hufnagl <lukas.hufnagl02@gmail.com>
2026-01-08 23:54:15 +0100

    Revert lazy loading - fix anchor links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,17 @@
 <template>
   <div class="relative z-10">
     <Hero />
-    <Suspense>
-      <About />
-      <template #fallback>
-        <div class="min-h-screen flex items-center justify-center">
-          <div class="animate-spin w-8 h-8 border-2 border-primary border-t-transparent rounded-full"></div>
-        </div>
-      </template>
-    </Suspense>
-    <Suspense>
-      <Timeline />
-    </Suspense>
-    <Suspense>
-      <Achievements />
-    </Suspense>
-    <Suspense>
-      <Contact />
-    </Suspense>
+    <About />
+    <Timeline />
+    <Achievements />
+    <Contact />
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
 import Hero from '../components/Hero.vue'
-
-// Lazy load components that are below the fold
-const About = defineAsyncComponent(() => import('../components/About.vue'))
-const Timeline = defineAsyncComponent(() => import('../components/Timeline.vue'))
-const Achievements = defineAsyncComponent(() => import('../components/Achievements.vue'))
-const Contact = defineAsyncComponent(() => import('../components/Contact.vue'))
+import About from '../components/About.vue'
+import Timeline from '../components/Timeline.vue'
+import Achievements from '../components/Achievements.vue'
+import Contact from '../components/Contact.vue'
 </script>
